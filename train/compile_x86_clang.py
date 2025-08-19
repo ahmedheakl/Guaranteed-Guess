b9c11a8767ee7d65ee3b96640646ffb8477e921f
Ahmed Heakl <52796111+ahmedheakl@users.noreply.github.com>
2025-08-19 15:52:14 +0200

    Create compile_x86_clang.py

---
+++ train/compile_x86_clang.py
@@ -0,0 +1,16 @@
+import subprocess
+import os
+
+def compile(input_file, optimization_level="O3", timeout=20) -> str:
+    base_output_file = input_file.replace(".c", "")
+    try:
+        asm_output = f"{base_output_file}_{optimization_level}_x86.s"
+        if os.path.exists(asm_output): return asm_output
+        command = f"clang -S -target x86_64-apple-darwin {input_file} -o {asm_output} -{optimization_level}"
+        _ = subprocess.run(
+            command, shell=True, check=True, capture_output=True, text=True, timeout=20
+        )
+        return asm_output
+    except Exception as e:
+        print(f"Error in file {input_file}: {e}")
+        return None
